c1ceb98725c097b71d67dbdf8e7a0b890730fb22
srinath duraisamy <srinath.duraisamy@intel.com>
2018-10-17 00:26:06 -0700

    added hear evaluation app

--- common/interpreter/cpp_processor/ContractDispatcher.cpp
+++ common/interpreter/cpp_processor/ContractDispatcher.cpp
@@ -18,10 +18,12 @@
 #include "work_order_data.h"
 #include "CppProcessor.h"
 #include "echo_work_order/EchoWorkOrder.h"
+#include "heart_disease_eval/heart_disease_evaluation.h"
 
 CppContractWrapper* intkey_factory();
 CppContractWrapper* echo_factory();
 pdo::contracts::ContractInterpreter* echo_result_factory();
+pdo::contracts::ContractInterpreter* heart_disease_eval_factory();
 
 ByteArray ConvertStringToByteArray(std::string s)
 {
@@ -66,9 +68,54 @@ public:
 
 } echo_result;
 
+class HeartDiseaseEvalFactory: public pdo::contracts::ContractInterpreterBase
+{
+public:
+	void process_work_order(
+		std::string code_id,
+		ByteArray tc_service_address,
+		ByteArray participant_address,
+		ByteArray enclave_id,
+		ByteArray work_order_id,
+		std::vector<pdo::WorkOrderData>& work_order_data)
+	{
+		std::string result_str;
+		pdo::WorkOrderData* message = 0;
+		pdo::WorkOrderData* result = 0;
+		for (size_t i = 0; i < work_order_data.size(); i++)
+		{
+			pdo::WorkOrderData& wo_data = work_order_data.at(i);
+			if (wo_data.data_type == "message")
+			{
+				message = &wo_data;
+			}
+			else
+			if (wo_data.data_type == "result")
+			{
+				result = &wo_data;
+			}
+		}
+		pdo::error::ThrowIfNull(message, "work order message not found");
+		pdo::error::ThrowIfNull(result, "work order result not found");
+
+		EchoResultImpl echo_result_impl;
+		result_str = executeWorkOrder(
+			ByteArrayToString(message->decrypted_input_data));
+
+		result->decrypted_output_data = ConvertStringToByteArray(result_str);
+	};
+
+} heart_disease_eval_worker;
+
+
 pdo::contracts::ContractInterpreter* echo_result_factory()
 {
 	return &echo_result;
+}
+
+pdo::contracts::ContractInterpreter* heart_disease_eval_factory()
+{
+	return &heart_disease_eval_worker;
 }
 
 ContractDispatchTableEntry contractDispatchTable[] = {
@@ -79,5 +126,6 @@ ContractDispatchTableEntry contractDispatchTable[] = {
 
 WorkOrderDispatchTableEntry workOrderDispatchTable[] = {
 	{"echo-result:", echo_result_factory},
+    { "heart-disease-eval:", heart_disease_eval_factory },
     {NULL, NULL}
 };
